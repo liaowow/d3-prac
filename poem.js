3d6dbc81160e3c4411664cc000bb0488071afe84
Annie Liao <liaowow@gmail.com>
2020-05-03 14:41:45 -0400

    added poem example

---
+++ poem.js
@@ -0,0 +1,19 @@
+// data to be manipulated
+let poemVerses = ["Always","in the middle", "of our bloodiest battles", "you lay down your arms","like flowering mines","to conquer me home."];
+
+/*** D3 Design Pattern ***/
+/* 1. create a selection */
+let selection = d3.select("#viz")
+                  .selectAll("p")
+
+selection
+  /* 2. associate data with selection */
+  .data(poemVerses)
+  .enter()
+  /* 3. append elements to the DOM */
+  .append("p")
+  /* 4. modify elements based on the data */
+  .text("Click Me To Reveal Verse")
+  .on("click", function(d, i) {
+    d3.select(this).text(d)
+  })
