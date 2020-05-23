9bd3da1d543a9ec8db8e5e4409d00d7ac84ad4c3
Annie Liao <liaowow@gmail.com>
2020-05-23 16:35:22 -0400

    added margin and translation

--- poem.js
+++ poem.js
@@ -64,3 +64,41 @@ scale.append("circle")
      .attr("cy", 100)
      .attr("r", 40)
      .style("fill", "green")
+
+/* practicing margin and translation */
+// set the dimensions and margins of the graph
+const margin = {
+  top: 10,
+  right: 40,
+  bottom: 30,
+  left: 30
+}
+
+const width = 450 - margin.left - margin.right
+const height = 400 - margin.top - margin.bottom
+
+// append the svg object to the body of the page
+const area = d3.select("#area")
+               .append("svg")
+               .attr("width", width + margin.left + margin.right)
+               .attr("height", height + margin.top + margin.bottom)
+               // translate this svg element to leave some margin
+               .append("g")
+               .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+// X scale and Axis
+const xForMargin = d3.scaleLinear()
+                     .domain([0, 100]) // This is the min and the max of the data: 0 to 100 if percentages
+                     .range([0, width]) // This is the corresponding value I want in Pixel
+
+area.append("g")
+    .attr("transform", "translate(0, " + height + ")")
+    .call(d3.axisBottom(xForMargin))
+
+// Y scale and Axis
+const yForMargin = d3.scaleLinear()
+                     .domain([0, 100]) // This is the min and the max of the data: 0 to 100 if percentages
+                     .range([height, 0]) // This is the corresponding value I want in Pixel
+
+area.append("g")
+    .call(d3.axisLeft(yForMargin))
