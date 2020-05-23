7d4b7f9af6075a83dde37a8bbd609944671d01aa
Annie Liao <liaowow@gmail.com>
2020-05-23 15:58:59 -0400

    added circles for scale

--- poem.js
+++ poem.js
@@ -23,7 +23,7 @@ d3.select(".target")
   .style("stroke-width", 7)
   // .style("opacity", 0.5)
 
-// practicing coordinates
+/* practicing coordinates */
 let coordinates = d3.select("#coordinate")
 coordinates.append("circle")
            .attr("cx", 20).attr("cy", 20).attr("r", 20).style("fill", "aqua")
@@ -31,3 +31,33 @@ coordinates.append("circle")
            .attr("cx", 140).attr("cy", 150).attr("r", 20).style("fill", "deeppink")
 coordinates.append("circle")
            .attr("cx", 280).attr("cy", 50).attr("r", 20).style("fill", "gold")
+
+/* practicing scales */
+// select the svg area
+const scale = d3.select("#scale")
+
+// create a scale: transform value in pixel
+const x = d3.scaleLinear()
+            .domain([0, 100]) // This is the min and the max of the data: 0 to 100 if percentages
+            .range([0, 400]) // This is the corresponding value I want in Pixel
+
+console.log(x(100))
+
+// Add 3 dots for 0, 50 and 100%
+scale.append("circle")
+     .attr("cx", x(10))
+     .attr("cy", 100)
+     .attr("r", 40)
+     .style("fill", "blue")
+
+scale.append("circle")
+     .attr("cx", x(50))
+     .attr("cy", 100)
+     .attr("r", 40)
+     .style("fill", "red")
+
+scale.append("circle")
+     .attr("cx", x(100))
+     .attr("cy", 100)
+     .attr("r", 40)
+     .style("fill", "green")
