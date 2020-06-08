1b58dc6f9d506312d88a7db28afa959646d52eab
Annie Liao <liaowow@gmail.com>
2020-06-08 16:10:52 -0400

    added script for axes styling

--- nycAir/index.js
+++ nycAir/index.js
@@ -17,7 +17,7 @@ const height = +svg.attr('height')
 const render = data => {
   const xValue = d => d.airquality
   const yValue = d => d.date
-  const margin = { top: 20, right: 20, bottom: 35, left: 100 }
+  const margin = { top: 20, right: 20, bottom: 40, left: 100 }
   const innerWidth = width - margin.left - margin.right
   const innerHeight = height - margin.top - margin.bottom
   
@@ -35,20 +35,33 @@ const render = data => {
   const yAxis = d3.axisLeft(yScale)
   
   const g = svg.append('g')
-  				.attr('transform', `translate(${margin.left}, ${margin.top})`)
+  				        .attr('transform', `translate(${margin.left}, ${margin.top})`)
   
-  yAxis(g.append('g'))
-  /* shorthand: g.append('g').call(yAxis) */
+  g.append('g').call(yAxis) // same as: yAxis(g.append('g'))
+      .selectAll('.domain, .tick line')
+      .remove()
   
-  g.append('g').call(d3.axisBottom(xScale))
+  const xAxis = d3.axisBottom(xScale)
+          .tickSize(-innerHeight)
+
+  const xAxisG = g.append('g').call(xAxis)
   	.attr('transform', `translate(0, ${innerHeight})`)
   
+  xAxisG.select('.domain').remove()
+
+  xAxisG.append('text')
+    .attr('y', 30)
+    .attr('x', 140)
+    .style('fill', 'navy')
+    .text('PM2.5 level: the lower the healthier')
+
   g.selectAll('rect').data(data)
   	.enter().append('rect')
   	.attr('y', d => yScale(yValue(d)))
   	.attr('width', d => xScale(xValue(d)))
     .attr('height', yScale.bandwidth())
-  	.style('fill', 'lavender')
+    .style('fill', 'lavender')
+    .style('opacity', 0.8)
 }
 
 d3.csv('data.csv').then((data) => {
