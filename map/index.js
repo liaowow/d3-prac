5527aa28ee5b5829bc8fe02c5efad3cc856d5b09
Annie Liao <liaowow@gmail.com>
2020-07-16 16:02:17 -0400

    added map interactions

--- map/index.js
+++ map/index.js
@@ -9,13 +9,23 @@ svg.append('path')
   .attr('class', 'sphere')
   .attr('d', pathGenerator({type: 'Sphere'}))
 
-d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
-  .then(data => {
-     const countries = topojson.feature(data, data.objects.countries)
-     const paths = svg.selectAll('path')
-             .data(countries.features)
-     paths.enter().append('path')
-       .attr('class', 'country')
-       .attr('d', d => pathGenerator(d))	
-  })
+
+Promise.all([
+  d3.tsv('https://unpkg.com/world-atlas@1.1.4/world/50m.tsv'),
+  d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
+]).then(([tsvData, jsonData]) => {
+  const countryName = tsvData.reduce((accumulator, d) => {
+    accumulator[d.iso_n3] = d.name
+    return accumulator
+  }, {})
+
+  const countries = topojson.feature(jsonData, jsonData.objects.countries)
+
+  svg.selectAll('path').data(countries.features)
+    .enter().append('path')
+      .attr('class', 'country')
+      .attr('d', d => pathGenerator(d))
+    .append('title')
+      .text(d => countryName[d.id])
+})
 
